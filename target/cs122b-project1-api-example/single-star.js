bdf83f24ead33bf42aacc1a2bb64a33ae9fec663
NiharikaKumar <nihakumar25@gmail.com>
2024-04-12 18:26:49 -0700

    made navigation bar for all pages; added more design to page titles

--- target/cs122b-project1-api-example/single-star.js
+++ target/cs122b-project1-api-example/single-star.js
@@ -45,7 +45,7 @@ function handleResult(resultData) {
     let starInfoElement = jQuery("#star_info");
 
     // append two html <p> created to the h3 body, which will refresh the page
-    starInfoElement.append("<h2>Star Name: " + resultData[0]["star_name"] + "</h2>" +
+    starInfoElement.append("<h2>Star Name: " + "<span style='color: #bb86fc;'>" + resultData[0]["star_name"] + "</span></h2>" +
         "<p>Date Of Birth: " + resultData[0]["star_dob"] + "</p>");
 
     console.log("handleResult: populating movie table from resultData");
