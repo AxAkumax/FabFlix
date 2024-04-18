a75627efab2358a92190574c62afe55ad6d0533d
AxAkumax <akshita.akumalla@gmail.com>
2024-04-18 14:49:15 -0700

    Login page completed-all links working and design implemented

--- src/LoginServlet.java
+++ src/LoginServlet.java
@@ -9,6 +9,7 @@ import javax.sql.DataSource;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
@@ -30,56 +31,91 @@ public class LoginServlet extends HttpServlet {
         }
     }
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws IOException {
+//    public void doGet(HttpServletRequest request, HttpServletResponse response)
+//            throws IOException {
+//
+//        // Content type and output stream remain the same
+//
+//        PrintWriter out = response.getWriter();
+//
+////        out.println("<html><head><title>Login</title></head>");
+////        out.println("<body><h1>Login</h1>");
+////
+////        // Display the login form
+////        out.println("<form action='./index.html' method='post'>");
+////        out.println("Email: <input type='text' name='email'><br>");
+////        out.println("Password: <input type='password' name='password'><br>");
+////        out.println("<input type='submit' value='Login'>");
+////        out.println("</form>");
+////
+////        out.println("</body></html>");
+//
+//        String email = request.getParameter("email");
+//        String password = request.getParameter("password");
+//
+//        try {
+//            Connection dbCon = dataSource.getConnection();
+//            Statement statement = dbCon.createStatement();
+//
+//            String query = String.format("SELECT * FROM customers WHERE email='%s' AND password='%s'", email, password);
+//            ResultSet rs = statement.executeQuery(query);
+//
+//            if (rs.next()) {
+//                // Authentication successful
+//                // Use request.getContextPath() to prepend the context path dynamically
+//                response.sendRedirect(request.getContextPath() + "/webcontent/index.html");
+//            } else {
+//                // Authentication failed
+//                response.sendRedirect(request.getContextPath() + "/webcontent/login.html");
+//                //response.sendRedirect("login.html");
+//            }
+//
+//            rs.close();
+//            statement.close();
+//            dbCon.close();
+//
+//        } catch (Exception e) {
+//            // Error handling
+//            e.printStackTrace();
+//        }
+//    }
+public void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws IOException {
 
-        // Content type and output stream remain the same
+    String email = request.getParameter("email");
+    String password = request.getParameter("password");
+    PrintWriter out = response.getWriter();
 
-        PrintWriter out = response.getWriter();
+    try {
+        Connection dbCon = dataSource.getConnection();
 
-        out.println("<html><head><title>Login</title></head>");
-        out.println("<body><h1>Login</h1>");
+        // Use prepared statement with parameterized query
+        String query = "SELECT * FROM customers WHERE email = ? AND password = ?";
+        PreparedStatement preparedStatement = dbCon.prepareStatement(query);
+        preparedStatement.setString(1, email);
+        preparedStatement.setString(2, password);
 
-        // Display the login form
-        out.println("<form action='/api/login' method='post'>");
-        out.println("Email: <input type='text' name='email'><br>");
-        out.println("Password: <input type='password' name='password'><br>");
-        out.println("<input type='submit' value='Login'>");
-        out.println("</form>");
+        ResultSet rs = preparedStatement.executeQuery();
 
-        out.println("</body></html>");
+        if (rs.next()) {
+            // Authentication successful
+            response.sendRedirect(request.getContextPath() + "/index.html");
+        } else {
+            // Authentication failed
+            response.sendRedirect(request.getContextPath() + "/login.html?error=true");
+        }
+
+        rs.close();
+        preparedStatement.close();
+        dbCon.close();
+
+    } catch (Exception e) {
+        // Error handling
+        e.printStackTrace();
     }
-
+}
     public void doPost(HttpServletRequest request, HttpServletResponse response)
             throws IOException {
-
-        String email = request.getParameter("email");
-        String password = request.getParameter("password");
-
-        try {
-            Connection dbCon = dataSource.getConnection();
-            Statement statement = dbCon.createStatement();
-
-            String query = String.format("SELECT * FROM customers WHERE email='%s' AND password='%s'", email, password);
-            ResultSet rs = statement.executeQuery(query);
-
-            if (rs.next()) {
-                // Authentication successful
-                // Use request.getContextPath() to prepend the context path dynamically
-                response.sendRedirect(request.getContextPath() + "/webcontent/index.html");
-            } else {
-                // Authentication failed
-                response.sendRedirect(request.getContextPath() + "/webcontent/form.html");
-                //response.sendRedirect("login.html");
-            }
-
-            rs.close();
-            statement.close();
-            dbCon.close();
-
-        } catch (Exception e) {
-            // Error handling
-            e.printStackTrace();
+            doGet(request, response);
         }
-    }
 }
