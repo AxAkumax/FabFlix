8e100a98a67fddec74fffe80f27ebac8c1684389
NiharikaKumar <nihakumar25@gmail.com>
2024-04-23 22:17:06 -0700

    able to get movies by genre (have to fix presentation for it) + changed the navbar

--- src/BrowseServlet.java
+++ src/BrowseServlet.java
@@ -1,5 +1,8 @@
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import jakarta.servlet.ServletConfig;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -27,9 +30,90 @@ public class BrowseServlet extends HttpServlet {
         }
     }
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws IOException {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        System.out.println("inside doget");
+        System.out.println(request.getQueryString());
 
+        response.setContentType("application/json"); // Response mime type
+        PrintWriter out = response.getWriter();
+
+        try (Connection conn = dataSource.getConnection()) {
+
+            String genreIdParameter = request.getParameter("genreId");
+
+            if (genreIdParameter != null) {
+                System.out.println("line 45 " + genreIdParameter);
+                int genreId = Integer.parseInt(genreIdParameter);
+                // Execute SQL query to get movies for the given genreId
+                String query = "SELECT genres.id, movies.title " +
+                                "FROM genres_in_movies " +
+                                "JOIN movies ON genres_in_movies.movieId = movies.id " +
+                                "JOIN genres ON genres_in_movies.genreId = genres.id " +
+                                "WHERE genres_in_movies.genreId = ?";
+
+                PreparedStatement statement = conn.prepareStatement(query);
+                statement.setInt(1, genreId);
+                ResultSet rs = statement.executeQuery();
+                JsonArray jsonArray = new JsonArray();
+                System.out.println(rs);
+                while (rs.next()) {
+                    String title = rs.getString("title");
+                    System.out.println(title);
+                    JsonObject jsonObject = new JsonObject();
+                    jsonObject.addProperty("title", title);
+                    jsonArray.add(jsonObject);
+                }
+
+                rs.close();
+                statement.close();
+
+                // Write JSON string to output
+                out.write(jsonArray.toString());
+                // Set response status to 200 (OK)
+                response.setStatus(200);
+            }
+            else {
+                String query = "SELECT id, name FROM genres ORDER BY name ASC;";
+
+                PreparedStatement statement = conn.prepareStatement(query);
+                ResultSet rs = statement.executeQuery();
+                JsonArray jsonArray = new JsonArray();
+
+                while (rs.next()) {
+                    String genreId = rs.getString("id");
+                    String genreName = rs.getString("name");
+
+                    JsonObject jsonObject = new JsonObject();
+                    jsonObject.addProperty("genreId", genreId);
+                    jsonObject.addProperty("genreName", genreName);
+
+                    jsonArray.add(jsonObject);
+                }
+
+                rs.close();
+                statement.close();
+
+                // Write JSON string to output
+                out.write(jsonArray.toString());
+                // Set response status to 200 (OK)
+                response.setStatus(200);
+            }
+        }
+        catch (Exception e) {
+            // Write error message JSON object to output
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("errorMessage", e.getMessage());
+            out.write(jsonObject.toString());
+
+            // Log error to localhost log
+            request.getServletContext().log("Error:", e);
+            // Set response status to 500 (Internal Server Error)
+            response.setStatus(500);
+        }
+        finally {
+            out.close();
+        }
     }
+
 
 }
--- webcontent/browse.js
+++ webcontent/browse.js
@@ -1,16 +1,77 @@
+// Function to handle click on genre link
+function handleGenreClick(event) {
+    event.preventDefault(); // Prevent default link behavior
+    let genreId = jQuery(this).data("genre-id");
+    console.log(genreId);
 
-function handleBrowseResult(resultData) {
+    // Make AJAX request to fetch genre movies
+    jQuery.ajax({
+        dataType: "json",
+        method: "GET",
+        url: "api/browse?genreId=" + genreId,
+        success: function(resultData) {
+            // Handle successful response
+            console.log("Genre movies:", resultData);
 
+            // Clear previous results
+            jQuery("#browsing_results").empty();
+
+            // Display genre movies below browsing section
+            let resultsHtml = "<h5>Genre Movies</h5><ul>";
+            for (let i = 0; i < resultData.length; i++) {
+                resultsHtml += "<li>" + resultData[i].title + "</li>";
+            }
+            resultsHtml += "</ul>";
+            jQuery("#browsing_results").html(resultsHtml);
+        },
+        error: function(xhr, status, error) {
+            // Handle error
+            console.error("Error fetching genre movies:", error);
+        }
+    });
 }
 
-/**
- * Once this .js is loaded, following scripts will be executed by the browser
- */
+// Function to populate genre table
+function handleBrowseResult(resultData) {
+    console.log("creating browse result");
+    console.log(resultData);
 
-// Makes the HTTP GET request and registers on success callback function handleStarResult
+    let genresElement = jQuery("#genre_table_body");
+    let row = "";
+
+    // Iterate through the resultData array
+    for (let i = 0; i < resultData.length; i++) {
+        // Add a new row for every third genre or at the beginning of the loop
+        if (i % 3 === 0) {
+            // Close the previous row if it exists
+            if (i !== 0) {
+                row += "</tr>";
+            }
+            // Start a new row
+            row += "<tr>";
+        }
+
+        row += "<td>" +
+            '<a href="#" class="genre-link" data-genre-id="' + resultData[i]['genreId'] + '">' +
+            resultData[i]["genreName"] +     // display genreName for the link text
+            '</a>' +
+            "</td>";
+    }
+
+    // Close the last row
+    row += "</tr>";
+
+    // Append the constructed row to the genresElement
+    genresElement.append(row);
+
+    // Add click event listener to genre links
+    jQuery(".genre-link").click(handleGenreClick);
+}
+
+// Makes the HTTP GET request and registers on success callback function handleBrowseResult
 jQuery.ajax({
-    dataType: "json", // Setting return data type
-    method: "GET", // Setting request method
-    url: "api/browse", // Setting request url, which is mapped by BrowseServlet in BrowseServlet.java
-    success: (resultData) => handleBrowseResult(resultData) // Setting callback function to handle data returned successfully by the StarsServlet
+    dataType: "json",
+    method: "GET",
+    url: "api/browse",
+    success: (resultData) => handleBrowseResult(resultData)
 });
