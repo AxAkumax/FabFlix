9fa181e8d7149c019e4da978141d8ce1174476cc
AxAkumax <akshita.akumalla@gmail.com>
2024-04-25 21:05:00 -0700

    Search table additions: stars alphabetically ordered

--- webcontent/search.js
+++ webcontent/search.js
@@ -41,39 +41,6 @@ function submitSearchForm(event) {
     });
 }
 
-// // Function to handle movie data and update the table
-// function handleMovieData(resultData) {
-//     console.log("handleMovieData: populating movie table from resultData");
-//     console.log(resultData);
-//
-//     // Clear existing table rows
-//     $("#movie_table_body").empty();
-//
-//     // Iterate through search results and populate table
-//     resultData.movies.forEach(function(movie) {
-//         // Construct HTML for table row
-//         var rowHTML = "<tr>";
-//         rowHTML += "<td>" + movie.title + "</td>";
-//         rowHTML += "<td>" + movie.year + "</td>";
-//         rowHTML += "<td>" + movie.director + "</td>";
-//         rowHTML += "<td>" + movie.genres + "</td>";
-//
-//         let star_id_names = movie.stars.split(";");
-//         let star_entry =
-//             // Add a link to single-star.html with id passed with GET url parameter
-//             '<a href="single-star.html?id=' + star_id_names[0] + '">'
-//             + star_id_names[1] +     // display star_name for the link text
-//             '</a>';
-//         // Display only star names
-//         rowHTML += "<td>" + star_entry + "</td>";
-//         //.split(';').join(', ')
-//         rowHTML += "<td>" + movie.rating + "</td>";
-//         rowHTML += "</tr>";
-//
-//         // Append row to table
-//         $("#movie_table_body").append(rowHTML);
-//     });
-// }
 // Function to handle movie data and update the table
 function handleMovieData(resultData) {
     console.log("handleMovieData: populating movie table from resultData");
@@ -92,22 +59,30 @@ function handleMovieData(resultData) {
         rowHTML += "<td>" + movie.genres + "</td>";
 
         let star_id_names = movie.stars.split(";");
-        let star_entry = "";
+        let stars = [];
 
-        // Iterate through each star ID and name pair
-        for(let i=0; i<star_id_names.length; i=i+2){
+        // Create star objects and push them into the stars array
+        for (let i = 0; i < star_id_names.length; i += 2) {
             let star_id = star_id_names[i];
-            let star_name = star_id_names[i+1];
+            let star_name = star_id_names[i + 1];
+            stars.push({ id: star_id, name: star_name });
+        }
 
-            // Add a link to single-star.html with id passed with GET url parameter
-            let star_link = '<a href="single-star.html?id=' + star_id + '">' + star_name + '</a>';
+        // Sort the stars array by star name in ascending order
+        stars.sort((a, b) => a.name.localeCompare(b.name));
 
-            // Append the hyperlink to the star_entry string
-            if(i+1 < star_id_names.length-1){
-                star_entry += star_link + ", ";
-            }
-            else{
-                star_entry += star_link;
+        // Initialize the star_entry string
+        let star_entry = '';
+
+        // Construct hyperlinks for sorted stars
+        for (let i = 0; i < stars.length; i++) {
+            let star = stars[i];
+            let star_link = '<a href="single-star.html?id=' + star.id + '">' + star.name + '</a>';
+            star_entry += star_link;
+
+            // Add comma and space if it's not the last star
+            if (i < stars.length - 1) {
+                star_entry += ", ";
             }
         }
 
@@ -120,7 +95,6 @@ function handleMovieData(resultData) {
         $("#movie_table_body").append(rowHTML);
     });
 }
-
 
 // Event listener for form submission
 $(document).ready(function() {
